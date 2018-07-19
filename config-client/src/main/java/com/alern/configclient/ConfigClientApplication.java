ca5e8598ab75dcc9d2c46a2ecb3fa27e4f4490e7
1349034503@qq.com <1349034503@qq.com>
2018-07-19 16:49:52 +0800

    修改配置

--- config-client/src/main/java/com/alern/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/alern/configclient/ConfigClientApplication.java
@@ -1,12 +1,27 @@
 package com.alern.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	//在config-server端有与客户端同名的配置文件才能获取到
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
 	}
 }
